a9542158518b09fee3edf1b8efacef03497c7564
Vanapalli Naveen Kumar <naveen.vanapalli@forsys.taas.com>
2019-08-22 21:18:40 +0530

    Added templates

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+# Create your models.
+    # Title
+    # Publication date
+    # content Body
+    # Image
+class Blog(models.Model):
+    title = models.CharField(max_length=255)
+    pub_date = models.DateField()
+    body = models.TextField()
+    image = models.ImageField(upload_to='images/')
+# Add to Blog app to the Settings
+# Create a migration
+# Migrate
+# Add to the admin
--- project2/settings.py
+++ project2/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'blog.apps.BlogConfig',
     'jobs.apps.JobsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
@@ -76,8 +77,14 @@ WSGI_APPLICATION = 'project2.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': 'mydatabase'
+        'ENGINE': 'django.db.backends.mysql', 
+        'NAME': 'portfolio',
+        'USER': 'root',
+        'PASSWORD': 'root',
+        'HOST': '127.0.0.1',   # Or an IP Address that your DB is hosted on
+        'PORT': '3306',
     }
 }
 

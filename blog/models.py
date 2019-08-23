35226982d0b624673a7a02318acda35f44316133
Vanapalli Naveen Kumar <naveen.vanapalli@forsys.taas.com>
2019-08-24 00:06:11 +0530

    Added Detail.html and static file folder using collectstatic command

--- blog/models.py
+++ blog/models.py
@@ -10,6 +10,15 @@ class Blog(models.Model):
     pub_date = models.DateField()
     body = models.TextField()
     image = models.ImageField(upload_to='images/')
+
+    def summary(self):
+        return self.body[:101]
+    
+    def pub_date_pretty(self):
+        return self.pub_date.strftime('%b %e %Y')
+
+    def __str__(self):
+        return self.title
 # Add to Blog app to the Settings
 # Create a migration
 # Migrate
